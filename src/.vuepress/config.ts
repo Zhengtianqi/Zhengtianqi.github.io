fb19e746db760a4a9311b8c7c7628da420fcc4e8
zhengtianqi <270490096@qq.com>
2026-06-08 13:42:58 +0800

    update(config) 更新

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -22,6 +22,9 @@ export default defineUserConfig({
     }),
     searchConsolePlugin({
       // options ...
+      toutiaoAutoPushId: "21fca8c35311bd613eb106b7c4596885ddfae59f479386b9ce21f9b588d5a55a8a24d2f724c31ebe20c1e4e6fba6d91caf576100b02a2870c72f38fc574066fef065d152c73bf1cbb2ebad3b5b5265d8",
+      autoPushBaiduSwitch: true,
+      autoPush360Switch: true,
     }),
   ],
   // Enable it with pwa
