c856a9742190bf533fa89aaa9f7c390ce2035825
zhengtianqi <270490096@qq.com>
2026-07-06 18:25:15 +0800

    feat(config) pnpm尝试

--- src/.vuepress/.temp/internal/siteData.js
+++ src/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,12 @@
 export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"郑天祺的博客\",\"description\":\"郑天祺的博客\",\"head\":[[\"meta\",{\"name\":\"bytedance-verification-code\",\"content\":\"zjHSprOdFlen2eSs8phv\"}]],\"locales\":{\"/\":{\"lang\":\"zh-CN\",\"title\":\"郑天祺的博客\",\"description\":\"郑天祺的博客\"}}}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  __VUE_HMR_RUNTIME__.updateSiteData?.(siteData)
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept((m) => {
+    __VUE_HMR_RUNTIME__.updateSiteData?.(m.siteData)
+  })
+}
