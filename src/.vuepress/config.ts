90989287c510e191e9524d0403c1b075fbe37ec0
zhengtianqi <zhengtq@bjcrg.com>
2026-09-10 15:34:40 +0800

    chore(config): 添加 Google 站点验证

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -11,7 +11,9 @@ export default defineUserConfig({
     // 头条站长验证
     ['meta', { name: 'bytedance-verification-code', content: 'zjHSprOdFlen2eSs8phv' }],
     // Bing 站点验证
-    ['meta', { name: 'msvalidate.01', content: 'FAA905D2EF753B2EBC07361E0934C152' }]
+    ['meta', { name: 'msvalidate.01', content: 'FAA905D2EF753B2EBC07361E0934C152' }],
+    // Google 站点验证
+    ['meta', { name: 'google-site-verification', content: 'e-r4NfQLQIKH9qKI_9emDFsuUuEqzPG3mo-ixX0lqyA' }]
   ],
   theme,
 });
