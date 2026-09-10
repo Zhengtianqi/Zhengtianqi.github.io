d92a8003072a357175dccef5d2390920a943e5f2
zhengtianqi <zhengtq@bjcrg.com>
2026-09-10 14:40:14 +0800

    chore(config): 添加 Bing 站点验证 meta 标签

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,7 +9,9 @@ export default defineUserConfig({
   description: "郑天祺的博客",
   head: [
     // 头条站长验证
-    ['meta', { name: 'bytedance-verification-code', content: 'zjHSprOdFlen2eSs8phv' }]
+    ['meta', { name: 'bytedance-verification-code', content: 'zjHSprOdFlen2eSs8phv' }],
+    // Bing 站点验证
+    ['meta', { name: 'msvalidate.01', content: 'FAA905D2EF753B2EBC07361E0934C152' }]
   ],
   theme,
 });
